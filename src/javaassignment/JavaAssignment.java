e3967131711e05fe787d8833e0d959b960768a72
webby813 <webbyteo02@gmail.com>
2024-06-22 10:30:41 +0800

    Second commit

--- src/javaassignment/JavaAssignment.java
+++ src/javaassignment/JavaAssignment.java
@@ -3,7 +3,6 @@ package javaassignment;
 public class JavaAssignment {
 
     public static void main(String[] args) {
-        System.out.println("Testing");
+        System.out.println("Testing 2");
     }
-    
 }
