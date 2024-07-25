c979284ba495fcc4af62d1427298707f80fc14c2
LAPTOP-K6C36UHF\cgy03 <cgy031215@gmail.com>
2024-07-25 10:32:21 +0800

    ignore testing

--- src/javaassignment/pages/UserRegis.java
+++ src/javaassignment/pages/UserRegis.java
@@ -175,22 +175,23 @@ public class UserRegis extends javax.swing.JFrame {
  
     
     public static void main(String args[]) {
-        try{
-            for(javax.swing.UIManager.LookAndFeelInfo info :javax.swing.UIManager.getInstalledLookAndFeels()){
-                if("Nimbus".equals(info.getName())){
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        }catch(ClassNotFoundException ex){
-            java.util.logging.Logger.getLogger(UserRegis.class.getName()).log(java.util.SEVERE,null,ex);
-        }catch(InstantiationException ex){
-            java.util.logging.Logger.getLogger(UserRegis.class.getName()).log(java.util.SEVERE,null,ex);
-        }catch(IllegalAccessException ex){
-            java.util.logging.Logger.getLogger(UserRegis.class.getName()).log(java.util.SEVERE,null,ex);
-        }catch(javax.swing.UnsupportedLookAndFeelException ex){
-            java.util.logging.Logger.getLogger(UserRegis.class.getName()).log(java.util.SEVERE,null,ex);
-        }
+        ///COMMIT TESTING
+//        try{
+//            for(javax.swing.UIManager.LookAndFeelInfo info :javax.swing.UIManager.getInstalledLookAndFeels()){
+//                if("Nimbus".equals(info.getName())){
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        }catch(ClassNotFoundException ex){
+//            java.util.logging.Logger.getLogger(UserRegis.class.getName()).log(java.util.SEVERE,null,ex);
+//        }catch(InstantiationException ex){
+//            java.util.logging.Logger.getLogger(UserRegis.class.getName()).log(java.util.SEVERE,null,ex);
+//        }catch(IllegalAccessException ex){
+//            java.util.logging.Logger.getLogger(UserRegis.class.getName()).log(java.util.SEVERE,null,ex);
+//        }catch(javax.swing.UnsupportedLookAndFeelException ex){
+//            java.util.logging.Logger.getLogger(UserRegis.class.getName()).log(java.util.SEVERE,null,ex);
+//        }
         
 
         /* Create and display the form */
